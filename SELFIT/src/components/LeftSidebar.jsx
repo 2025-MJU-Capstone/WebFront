b14ddc4724e949098054c13b4ae96c74f13c57a3
MEspeaker <sho3492@gmail.com>
2025-04-27 00:22:43 +0900

    Update: sidebar button
    사이드바 버튼 누를때 이미지 색상바뀌게

--- SELFIT/src/components/LeftSidebar.jsx
+++ SELFIT/src/components/LeftSidebar.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom'
-import Body from "../assets/BOdy.svg"
+import { useState } from 'react';
+import Body from "../assets/Body.svg"
 import Closet from "../assets/Closet.svg"
 import Search from "../assets/Search.svg"
 import Setting from "../assets/Setting.svg"
+import sBody from "../assets/select_Body.svg"
+import sCloset from "../assets/select_Closet.svg"
+import sSearch from "../assets/select_Search.svg"
+import sSetting from "../assets/select_Setting.svg"
 
 function LeftSidebar({ setMode }) {
+  const [selectedButton, setSelectedButton] = useState(null);
   return (
     <div style={{
       width: '70px',
@@ -19,38 +25,42 @@ function LeftSidebar({ setMode }) {
       <h3>메뉴</h3>
       
       <img
-          src={Body}
+          src={selectedButton === 'body' ? sBody : Body}
           alt="Body"
           width="40px"
           style={{ cursor: "pointer" }}
           onClick={() => {
+            setSelectedButton('body');
             setMode('input')
           }}
         />
       <img
-          src={Search}
+          src={selectedButton === 'search' ? sSearch : Search}
           alt="Store"
           width="40px"
           style={{ cursor: "pointer" }}
           onClick={() => {
+            setSelectedButton('search');
             setMode('iframe')
           }}
         />
        <img
-          src={Closet}
+          src={selectedButton === 'closet' ? sCloset : Closet}
           alt="Closet"
           width="40px"
           style={{ cursor: "pointer" }}
           onClick={() => {
+            setSelectedButton('closet');
             setMode('somethingElse')
           }}
         /> 
       <img
-          src={Setting}
+          src={selectedButton === 'setting' ? sSetting : Setting}
           alt="Setting"
           width="40px"
           style={{ cursor: "pointer" }}
           onClick={() => {
+            setSelectedButton('setting');
             setMode('anotherThing')
           }}
         />
